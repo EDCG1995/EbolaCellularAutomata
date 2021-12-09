82400494161e9bb3edf656b2328920b000e73c62
Enzo Corsini <b00129517@mytudublin.ie>
2021-12-09 15:29:33 +0000

    .

--- CellularAutomata/neighbourhood.c
+++ CellularAutomata/neighbourhood.c
@@ -12,27 +12,35 @@ FILE NOT READY, NEEDS TO BE WORKED ON:
 
 extern int world[Xaxis][Yaxis];
 extern int temp[Xaxis][Yaxis];
-
+extern int piece;
 void *test(void *rank)
 {
+
+    /*
+    thread 0 will check 0 to 249
+    thread 1 will check 250 to 499
+    thread 2 will check 500 to 749
+    thread 3 will check 750 999
+    
+    */
     long r = (long)rank;
-    switch (r)
+    long check = r * piece;
+    if (check == 0)
     {
-    case 0:
-        printf("thread 0");
-        break;
-    case 1:
-        printf("thread 1");
-        break;
-    case 2:
-        printf("thread 2");
-        break;
-    
-    default:
-    printf("thread 3");
-        break;
+        checkNeighbourhood(0, 249);
     }
-    
+    if (check == 250)
+    {
+        checkNeighbourhood(250, 499);
+    }
+    if (check == 500)
+    {
+        checkNeighbourhood(500, 749);
+    }
+    if (check == 750)
+    {
+        checkNeighbourhood(750, 999);
+    }
 }
 
 float setInfectionRate(int infectedNeighbours, int deceased)
@@ -70,12 +78,20 @@ float setInfectionRate(int infectedNeighbours, int deceased)
         infectionRate = 0.0;
     }
     infectionRate += (deceased * 0.05); //the infection rate increases 5% for every infected neighbour
-
+    printf("inf neigh= %d, dec= %d, infRate = %f \n", infectedNeighbours, deceased, infectionRate);
     //Return infectionRate;
 }
 
-void checkNeighbourhood(int row, int col)
+void checkNeighbourhood(int lowerBoundary, int upperBoundary)
 {
+    int summ = 0;
+    for (lowerBoundary; lowerBoundary <= upperBoundary; lowerBoundary++)
+    {
+        summ++;
+    }
+    printf("total amount %d", summ);
+    int row = 0; //change this bitch
+    int col = 0; //change this bitch
     int infNeigh = 0;
     int dead = 0;
 
